4f5ef6093db679dab1c9eaa6f0e0362519db57e4
detierrez <detierrez@gmail.com>
2026-03-02 22:17:18 -0300

    Delete unrelated files

--- controllers/validation/categories.js
+++
@@ -1,9 +0,0 @@
-const { param, body } = require("express-validator");
-const { raiseErrors, attachErrors } = require("./common");
-
-module.exports.validateId = [param("id").isInt(), raiseErrors];
-
-module.exports.validateName = [
-  body("name").exists({ values: "falsy" }).isLength({ max: 32 }),
-  attachErrors,
-];
