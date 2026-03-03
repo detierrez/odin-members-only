5a7dfcb1e6e8035d93b820bfadadf97abcc09be7
detierrez <detierrez@gmail.com>
2026-03-03 12:31:02 -0300

    Refactor name

--- controllers/validation/users.js
+++ controllers/validation/users.js
@@ -3,23 +3,23 @@ const { attachErrors, raiseErrors } = require("./common");
 const db = require("../../db/queries");
 
 module.exports.validateUser = [
-  commonValidations("username").custom(async (username) => {
+  commonlyValidate("username").custom(async (username) => {
     if (await db.hasUsername(username)) {
       throw new Error("username already exists");
     }
   }),
-  commonValidations("password")
+  commonlyValidate("password")
     .isLength({ min: 8 })
     .withMessage("password must be at least 8 characters long"),
-  commonValidations("passwordConfirm")
+  commonlyValidate("passwordConfirm")
     .custom((value, { req }) => value === req.body.password)
     .withMessage("passwords must match"),
-  commonValidations("firstName"),
-  commonValidations("lastName"),
+  commonlyValidate("firstName"),
+  commonlyValidate("lastName"),
   attachErrors,
 ];
 
-function commonValidations(field) {
+function commonlyValidate(field) {
   return body(field)
     .trim()
     .exists({ values: "falsy" })
